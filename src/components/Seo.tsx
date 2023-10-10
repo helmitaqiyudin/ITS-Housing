090800d3cce460a7091c7fc169f53ff88264d805
Helmi Taqiyudin <helmitaqiyudin@gmail.com>
2023-10-10 20:17:01 +0700

    feat:seo

---
+++ src/components/Seo.tsx
@@ -0,0 +1,39 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+const defaultMeta = {
+  title: 'ITS Housing',
+  siteName: 'ITS Housing',
+  description:
+    '',
+  url: '',
+  type: 'website',
+  robots: 'follow, index',
+  image: '',
+};
+
+type SeoProps = {
+  date?: string;
+  templateTitle?: string;
+} & Partial<typeof defaultMeta>;
+
+export default function Seo(props: SeoProps) {
+  const router = useRouter();
+  const meta = {
+    ...defaultMeta,
+    ...props,
+  };
+  meta.title = props.templateTitle
+    ? `${props.templateTitle} | ${meta.siteName}`
+    : meta.title;
+
+  return (
+    <Head>
+      <title>{meta.title}</title>
+      <meta name='robots' content={meta.robots} />
+      <meta content={meta.description} name='description' />
+      <meta property='og:url' content={`${meta.url}${router.asPath}`} />
+      <link rel='canonical' href={`${meta.url}${router.asPath}`} />
+    </Head>
+  );
+}

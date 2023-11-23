d2702c0254c1d555aeb290764985a357ee11e209
Helmi Taqiyudin <helmitaqiyudin@gmail.com>
2023-11-23 09:18:40 +0700

    fix: admin request url

--- src/components/tablecolumns/adminrequestcolumns.tsx
+++ src/components/tablecolumns/adminrequestcolumns.tsx
@@ -34,7 +34,7 @@ function ActionsCell({ row }: ActionsCellProps) {
     const router = useRouter();
 
     const handleViewDetail = () => {
-        void router.push(`/user/my-request/${row.original.id}`);
+        void router.push(`/admin/manage-requests/${row.original.id}`);
     };
 
     return (
